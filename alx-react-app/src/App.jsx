fd533d89a796980ae6ac05aee377553e88288a64
Nigel Dolling <ndolling5@gmail.com>
2025-08-31 19:51:38 +0000

    added user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
@@ -15,6 +16,11 @@ function App() {
       <Header />
       <MainContent>
         <WelcomeMessage />
+        <UserProfile 
+          name="Alice" 
+          age={25} 
+          bio="Loves hiking and photography" 
+        />
         <div>
           <a href="https://vite.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
